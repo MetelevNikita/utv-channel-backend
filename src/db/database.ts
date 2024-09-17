f2cb265bec26abf82aec1e77890d89a1f06ab93f
MetelevNikita <Kyle.B@mail.ru>
2024-09-17 16:26:21 +0500

    fixed url db

--- src/db/database.ts
+++ src/db/database.ts
@@ -4,9 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const pool = new Pool({
-  user: process.env.DB_TEST_NAME,
-  host: process.env.DB_TEST_HOST,
-  database: process.env.DB_TEST_BASE,
-  password: process.env.DB_TEST_PASSWORD,
-  port: process.env.DB_TEST_PORT as any,
+  user: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_BASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT as any,
 })

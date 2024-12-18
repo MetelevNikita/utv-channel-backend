24b2ba463a4cda10dfaac6b45c6de35949eb57d7
MetelevNikita <Kyle.B@mail.ru>
2024-12-18 10:05:00 +0500

    fix error title_image v3.0

--- public/js/app.js
+++ public/js/app.js
@@ -653,7 +653,7 @@ newsForm.addEventListener('submit', async (e) => {
     newNewsForm.append('author', newsAuthor);
     newNewsForm.append('date', newsDate);
     newNewsForm.append('video', (newsVideo === null) ? '' : newsVideo.value);
-    newNewsForm.append('title_image', (newsTitleImage === null) ? '' : newsTitleImage.files[0]);
+    newNewsForm.append('title_image', (!newsTitleImage) ? '' : newsTitleImage.files[0]);
     newNewsForm.append('tags', tags);
     newNewsForm.append('views', views)
     newNewsForm.append('news_description', newsDescription)
